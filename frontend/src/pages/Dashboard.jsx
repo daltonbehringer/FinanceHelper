26cf855dda0ee0a3fffd5e3abbeff29de44ae196
Dalton Behringer <dalton.behringer@gmail.com>
2026-03-17 08:12:13 -0700

    Add Advisor Chat component to Dashboard for account updates

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -263,6 +263,9 @@ export default function Dashboard() {
         </Card>
       </div>
 
+      {/* Advisor Chat */}
+      <AdvisorChat accounts={accounts} onUpdate={refetchAccounts} />
+
       {/* Account sections */}
       <div className="grid grid-cols-1 lg:grid-cols-2 gap-8">
         {/* Debts */}
@@ -335,9 +338,6 @@ export default function Dashboard() {
           </CardBody>
         </Card>
       )}
-
-      {/* Advisor Chat */}
-      <AdvisorChat accounts={accounts} onUpdate={refetchAccounts} />
     </div>
   )
 }
